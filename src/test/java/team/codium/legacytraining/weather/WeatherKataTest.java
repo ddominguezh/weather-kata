0f120cc5aec3a78fbb2b0a3e4addaca062d6894a
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 00:00:33 +0200

    mock weatherrepository

--- src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
+++ src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,11 +20,13 @@ public class WeatherKataTest {
     // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weathercode,windspeed_10m_max&timezone=Europe%2FBerlin
 
     private CityRepository cityRepository;
-    
+    private WeatherRepository weatherRepository;
     @BeforeEach
     public void setUp() throws IOException{
         this.cityRepository = mock(CityRepository.class);
         when(this.cityRepository.coordinateOf(anyString())).thenReturn(Coordinate.create((float)41.400347, (float)2.159592));
+
+        this.weatherRepository = mock(WeatherRepository.class);
     }
 
     @Test
@@ -32,11 +36,13 @@ public class WeatherKataTest {
     
     @Test
     public void get_prediction_from_barcelona_today() throws IOException{
+        when(this.weatherRepository.predictionBy(any(Coordinate.class), any(LocalDate.class), anyBoolean())).thenReturn(Prediction.create("Rain: Slight, moderate and heavy intensity"));
         assertEquals(Prediction.create("Rain: Slight, moderate and heavy intensity"), new Forecast(this.cityRepository, new OpenMeteoApiWeatherRepository()).predict("Barcelona", null, false));
     }
 
     @Test
     public void get_wind_prediction_from_barcelona_today() throws IOException{
+        when(this.weatherRepository.predictionBy(any(Coordinate.class), any(LocalDate.class), anyBoolean())).thenReturn(Prediction.create("15.7"));
         assertEquals(Prediction.create("15.7"), new Forecast(this.cityRepository, new OpenMeteoApiWeatherRepository()).predict("Barcelona", null, true));
     }
 }
