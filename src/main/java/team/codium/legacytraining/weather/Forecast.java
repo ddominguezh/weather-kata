2566da1f6853a1537c82dcb6c94b3bacbcefef31
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 22:45:45 +0200

    create class Prediction and move method codeToText

--- src/main/java/team/codium/legacytraining/weather/Forecast.java
+++ src/main/java/team/codium/legacytraining/weather/Forecast.java
@@ -50,7 +50,7 @@ public class Forecast {
                         return String.valueOf(results.getJSONArray("windspeed_10m_max").getFloat(i));
                     } else {
                         int weatherCode = results.getJSONArray("weathercode").getInt(i);
-                        return codeToText(weatherCode);
+                        return Prediction.create(weatherCode).value();
                     }
                 }
             }
@@ -58,41 +58,5 @@ public class Forecast {
             return "";
         }
         return "";
-    }
-
-    private String codeToText(int weatherCode) {
-        Map<Integer, String> translation = new HashMap<Integer, String>() {
-            {
-                put(0, "lear sky");
-                put(1, "Mainly clear, partly cloudy, and overcast");
-                put(2, "Mainly clear, partly cloudy, and overcast");
-                put(3, "Mainly clear, partly cloudy, and overcast");
-                put(45, "Fog and depositing rime fog");
-                put(48, "Fog and depositing rime fog");
-                put(51, "Drizzle: Light, moderate, and dense intensity");
-                put(53, "Drizzle: Light, moderate, and dense intensity");
-                put(55, "Drizzle: Light, moderate, and dense intensity");
-                put(56, "Freezing Drizzle: Light and dense intensity");
-                put(57, "Freezing Drizzle: Light and dense intensity");
-                put(61, "Rain: Slight, moderate and heavy intensity");
-                put(63, "Rain: Slight, moderate and heavy intensity");
-                put(65, "Rain: Slight, moderate and heavy intensity");
-                put(66, "Freezing Rain: Light and heavy intensity");
-                put(67, "Freezing Rain: Light and heavy intensity");
-                put(71, "Snow fall: Slight, moderate, and heavy intensity");
-                put(73, "Snow fall: Slight, moderate, and heavy intensity");
-                put(75, "Snow fall: Slight, moderate, and heavy intensity");
-                put(77, "Snow grains");
-                put(80, "Rain showers: Slight, moderate, and violent");
-                put(81, "Rain showers: Slight, moderate, and violent");
-                put(82, "Rain showers: Slight, moderate, and violent");
-                put(85, "Snow showers slight and heavy");
-                put(86, "Snow showers slight and heavy");
-                put(95, "Thunderstorm: Slight or moderate");
-                put(96, "Thunderstorm with slight and heavy hail");
-                put(99, "Thunderstorm with slight and heavy hail");
-            }
-        };
-        return translation.get(weatherCode);
     }
 }
