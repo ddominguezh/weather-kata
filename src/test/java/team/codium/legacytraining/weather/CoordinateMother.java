acfeda5fe163df04b08e93b9c6dcf8054840cc92
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-20 00:07:06 +0200

    extract barcelona coordinates to new class

---
+++ src/test/java/team/codium/legacytraining/weather/CoordinateMother.java
@@ -0,0 +1,8 @@
+package team.codium.legacytraining.weather;
+
+public class CoordinateMother {
+    
+    public static Coordinate for_barcelona(){
+        return Coordinate.create((float)41.400347, (float)2.159592);
+    }
+}
