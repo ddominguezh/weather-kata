7f76b1f0a35994c2d7e6f09c62642cc2bffff029
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 23:43:58 +0200

    create test open meteo api

---
+++ src/test/java/team/codium/legacytraining/weather/OpenMeteoApiWeatherRepositoryTest.java
@@ -0,0 +1,18 @@
+package team.codium.legacytraining.weather;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+public class OpenMeteoApiWeatherRepositoryTest {
+    
+    @Test
+    public void get_predictions_for_barcelona() throws IOException{
+        OpenMeteoApiWeatherRepository repository = new OpenMeteoApiWeatherRepository();
+        assertNotEquals(Prediction.NULL(), repository.predictionBy(Coordinate.create((float)41.400347, (float)2.159592), LocalDate.now(), false));
+    }
+}
