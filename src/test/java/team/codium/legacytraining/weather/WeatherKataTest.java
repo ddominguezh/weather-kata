908a0fd9a36b913b77f8115934f452c7f0c35f73
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 23:29:54 +0200

    mock city repository

--- src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
+++ src/test/java/team/codium/legacytraining/weather/WeatherKataTest.java
@@ -1,5 +1,7 @@
 package team.codium.legacytraining.weather;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,23 +9,34 @@ import java.time.LocalDate;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WeatherKataTest {
     // https://positionstack.com/geo_api.php?query=Barcelona
     // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weathercode,windspeed_10m_max&timezone=Europe%2FBerlin
 
+    private CityRepository cityRepository;
+    
+    @BeforeEach
+    public void setUp() throws IOException{
+        this.cityRepository = mock(CityRepository.class);
+        when(this.cityRepository.coordinateOf(anyString())).thenReturn(Coordinate.create((float)41.400347, (float)2.159592));
+    }
+
     @Test
     public void predictions_empty_when_the_date_is_greater_than_seven_days_from_today() throws IOException{
-        assertEquals(Prediction.NULL(), new Forecast(new GeoApiCityRepository()).predict(null, LocalDate.now().plusDays(7), false));
+        assertEquals(Prediction.NULL(), new Forecast(this.cityRepository).predict(null, LocalDate.now().plusDays(7), false));
     }
     
     @Test
     public void get_prediction_from_barcelona_today() throws IOException{
-        assertEquals(Prediction.create("Rain: Slight, moderate and heavy intensity"), new Forecast(new GeoApiCityRepository()).predict("Barcelona", null, false));
+        assertEquals(Prediction.create("Rain: Slight, moderate and heavy intensity"), new Forecast(this.cityRepository).predict("Barcelona", null, false));
     }
 
     @Test
     public void get_wind_prediction_from_barcelona_today() throws IOException{
-        assertEquals(Prediction.create("15.7"), new Forecast(new GeoApiCityRepository()).predict("Barcelona", null, true));
+        assertEquals(Prediction.create("15.7"), new Forecast(this.cityRepository).predict("Barcelona", null, true));
     }
 }
